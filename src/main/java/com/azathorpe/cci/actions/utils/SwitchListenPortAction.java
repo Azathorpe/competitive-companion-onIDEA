a6fc8dcbc566069ff6c53dd30ed26ac6bc533149
Azazel <384735481@qq.com>
2026-01-26 23:00:17 +0800

    [feat] add new action
    when you right clike in anywhere.you can switch your status.

---
+++ src/main/java/com/azathorpe/cci/actions/utils/SwitchListenPortAction.java
@@ -0,0 +1,24 @@
+package com.azathorpe.cci.actions.utils;
+
+import com.azathorpe.cci.service.SocketService;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.ui.Messages;
+
+
+public class SwitchListenPortAction extends AnAction {
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
+        if(SocketService.isIsRunning()) {
+            SocketService.stopServer();
+            Messages.showInfoMessage("Stopped the Service", "Listen Port Func Is Stopped..");
+            System.out.println("Stopped the Service");
+        }
+        else {
+            SocketService.startServer();
+            Messages.showInfoMessage("Start the Service", "Listen Port Func Is Started..");
+            System.out.println("Start the Service");
+        }
+    }
+}
