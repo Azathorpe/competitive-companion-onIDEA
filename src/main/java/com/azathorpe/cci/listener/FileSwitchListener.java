01befd237fb929cd0cc1fdd3c9ba7817b04533b9
Azazel <384735481@qq.com>
2026-01-26 23:04:58 +0800

    [feat] add new func which can listen user switch the file

---
+++ src/main/java/com/azathorpe/cci/listener/FileSwitchListener.java
@@ -0,0 +1,33 @@
+package com.azathorpe.cci.listener;
+
+import com.intellij.openapi.components.Service;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.fileEditor.FileEditorManagerEvent;
+import com.intellij.openapi.fileEditor.FileEditorManagerListener;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * 当文件被切换时，我们刷新侧边栏的数据
+ */
+public class FileSwitchListener implements FileEditorManagerListener {
+
+    @Override
+    public void selectionChanged(@NotNull FileEditorManagerEvent event) {
+//        FileEditorManagerListener.super.selectionChanged(event);
+        System.out.println("File was changed");
+        System.out.println("new" + event.getNewFile().getPath());
+    }
+
+    @Override
+    public void fileOpened(@NotNull FileEditorManager source, @NotNull VirtualFile file) {
+        FileEditorManagerListener.super.fileOpened(source, file);
+    }
+
+    @Override
+    public void fileClosed(@NotNull FileEditorManager source, @NotNull VirtualFile file) {
+        FileEditorManagerListener.super.fileClosed(source, file);
+    }
+
+}
