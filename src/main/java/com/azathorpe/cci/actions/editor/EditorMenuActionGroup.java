e96cbb637782482e9c97d9bc1efd5680f93922bb
Azazel <384735481@qq.com>
2026-01-26 16:11:46 +0800

    [feat] i don't know what this class mean

---
+++ src/main/java/com/azathorpe/cci/actions/editor/EditorMenuActionGroup.java
@@ -0,0 +1,28 @@
+package com.azathorpe.cci.actions.editor;
+
+import com.intellij.openapi.actionSystem.ActionUpdateThread;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.DefaultActionGroup;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.NotNull;
+
+public class EditorMenuActionGroup extends DefaultActionGroup {
+    @Override
+    public void update(AnActionEvent e) {
+        VirtualFile vf = e.getData(PlatformDataKeys.VIRTUAL_FILE);
+        boolean menuAllowed = false;
+        if (vf != null) {
+//            LeetcodeEditor leetcodeEditor = ProjectConfig.getInstance(e.getProject()).getEditor(vf.getPath());
+//            if (leetcodeEditor != null) {
+//                menuAllowed = true;
+//            }
+        }
+        e.getPresentation().setEnabledAndVisible(menuAllowed);
+    }
+
+    @Override
+    public @NotNull ActionUpdateThread getActionUpdateThread() {
+        return  ActionUpdateThread.BGT;
+    }
+}
