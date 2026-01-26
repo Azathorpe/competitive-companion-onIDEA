a2d19855ba5d2dede1f1193a8b43c612de81b8e1
Azazel <384735481@qq.com>
2026-01-26 16:08:49 +0800

    [fix] improve SocketService
    Add a new func[stopServer],When you call startServer,set isRunning to true
    I don't know it work or not....Maybe later we should delete it :(. i am not a multithread's programmer.

---
+++ src/main/java/com/azathorpe/cci/service/SocketService.java
@@ -0,0 +1,66 @@
+package com.azathorpe.cci.service;
+
+import com.alibaba.fastjson2.JSON;
+import com.azathorpe.cci.model.Question;
+import com.intellij.openapi.components.Service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SocketService {
+    private static final int port = 10043;
+
+    private static boolean isRunning = false;
+
+    public static void startServer(){
+        if(!isRunning){
+            isRunning = true;
+            Thread thread = new Thread(() -> {
+                try(ServerSocket client = new ServerSocket(port)){
+                    while (isRunning){
+                        try(Socket server = client.accept()){
+                            System.out.println("New connection accepted " + server.getInetAddress() + ":" + server.getPort());
+                            onReceive(server);
+                        }
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }, "SocketService-Thread");
+            thread.start();
+        }
+    }
+
+    /**
+     * Stop Server at any time
+     */
+    public static void stopServer(){
+        isRunning = false;
+    }
+
+    // When the port is received json from Competitive Companion
+    // Do it own work
+    public static void onReceive(Socket client){
+        StringBuilder builder = new StringBuilder();
+        try{
+            BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            String line;
+            while ((line = in.readLine()) != null) {
+                if(line.contains("{") && line.contains("}"))
+                    builder.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(builder.toString());
+        Question message = JSON.parseObject(builder.toString(), Question.class);
+        System.out.println(message);
+        System.out.println(message.getTestCases());
+    }
+
+
+}
