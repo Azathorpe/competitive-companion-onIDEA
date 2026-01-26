3f702a2e98dd2d84823a10a336a6787644df5e7c
Azazel <384735481@qq.com>
2026-01-26 16:12:30 +0800

    [feat] Add an IconManager like...

---
+++ src/main/java/com/azathorpe/cci/icons/CCIIcons.java
@@ -0,0 +1,12 @@
+package com.azathorpe.cci.icons;
+
+import com.intellij.openapi.util.IconLoader;
+import com.intellij.ui.IconManager;
+import com.intellij.xml.frontback.impl.icons.XmlFrontbackImplIcons;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+public final class CCIIcons {
+    public static final @NotNull Icon CCI_ICON = IconLoader.getIcon("/META-INF/pluginIcon.svg", CCIIcons.class);
+}
