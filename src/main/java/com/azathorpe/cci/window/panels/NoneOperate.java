9d20a79e07f685c305e2ce556a542b4abda9db01
Azazel <384735481@qq.com>
2026-01-26 23:03:48 +0800

    [feat] add new JPanel

---
+++ src/main/java/com/azathorpe/cci/window/panels/NoneOperate.java
@@ -0,0 +1,12 @@
+package com.azathorpe.cci.window.panels;
+
+import javax.swing.*;
+
+/**
+ * When you do not want to perform any operation, use this class as a placeholder.
+ */
+public class NoneOperate extends JPanel {
+    public NoneOperate() {
+        super();
+    }
+}
