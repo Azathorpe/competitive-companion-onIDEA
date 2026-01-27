ce5d0b689f1d8b60c56d671b6f4c8e24e8761685
Azazel <384735481@qq.com>
2026-01-27 13:30:28 +0800

    [feat] PersistenceStorage Func

---
+++ src/main/java/com/azathorpe/cci/actions/utils/PersistenceStorage.java
@@ -0,0 +1,134 @@
+package com.azathorpe.cci.actions.utils;
+
+import com.alibaba.fastjson2.JSON;
+import com.azathorpe.cci.model.Question;
+import com.azathorpe.cci.model.Settings;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+/**
+ * @author Azathorpe
+ * @version 1.0
+ */
+public class PersistenceStorage {
+    public static final String PROPERTIES_FILE_PATH = System.getProperty("user.home") + "/.cci/properties.json";
+    /**
+     * 初始化前禁止使用
+     */
+    public static String CODE_TEMPLATE_FILE_PATH = System.getProperty("user.home") + "/.cci/";
+
+    public static final String AUTO_FETCH_PROBLEM = "auto_fetch_problem";
+
+    public static Settings settings = new Settings();
+
+    /**
+     * 存放问题集的目录
+     */
+    public static String QUESTION_FOLDER_PATH = "";
+    /**
+     * 存放实际代码的目录
+     */
+    public static String QUESTION_SRC_FOLDER_PATH = "";
+
+    /**
+     * Save question file to local storage
+     * @param question Question object
+     */
+    public static void saveQuestionFile(Question question){
+        String filePath = QUESTION_FOLDER_PATH + "/" + question.getGroup() + "/" + question.getName() + ".json";
+        File file = checkFileExist(filePath);
+
+        //Write JSON to file
+        try(FileWriter fileWriter = new FileWriter(file)) {
+            fileWriter.write(question.getTestCases());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        saveSolvedFile(question.getName(), question.getGroup());
+    }
+
+    /**
+     * Save solved file template to local storage
+     * @param questionName Question name
+     * @param questionGroup Question group
+     */
+    public static void saveSolvedFile(String questionName,String questionGroup){
+        //TODO: 根据不同语言选择不同模板
+        System.out.println("Saving solved file for " + questionName);
+        String filePath = QUESTION_SRC_FOLDER_PATH + questionGroup + "/" + questionName + ".java";
+        File file = checkFileExist(filePath);
+        //复制Template文件内容到Solved文件
+        try {
+            Files.copy(Path.of(getCODE_TEMPLATE_FILE_PATH()), file.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            System.out.println("Copied template to " + filePath);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 储存设置文件
+     */
+    public static void savePropertiesFile(){
+        checkFileExist(PROPERTIES_FILE_PATH);
+        try(FileWriter fw = new FileWriter(PROPERTIES_FILE_PATH)){
+            fw.write(JSON.toJSONString(settings));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 加载设置文件
+     */
+    public static void loadPropertiesFile(){
+        checkFileExist(PROPERTIES_FILE_PATH);
+        StringBuilder sb = new StringBuilder();
+        try(InputStream is = new FileInputStream(PROPERTIES_FILE_PATH)){
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        settings = JSON.parseObject(sb.toString(), Settings.class);
+
+        // 初始化CODE_TEMPLATE路径
+//        CODE_TEMPLATE_FILE_PATH += settings.getLanguage()+".template";
+    }
+
+    /**
+     * Check if file exists, if not create it
+     * @param filePath filepath
+     * @return File object
+     */
+    private static File checkFileExist(String filePath) {
+        File file = new File(filePath);
+
+        if(!file.exists()){
+            try {
+                Files.createDirectories(Paths.get(file.getParent()));
+                Files.createFile(Paths.get(filePath));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return file;
+    }
+
+    public static void initCodeTemplateFile(String language) {
+        checkFileExist(CODE_TEMPLATE_FILE_PATH + language + ".template");
+    }
+
+    public static String getCODE_TEMPLATE_FILE_PATH(){
+        return CODE_TEMPLATE_FILE_PATH + settings.getLanguage()+".template";
+    }
+}
--- src/main/java/com/azathorpe/cci/window/WindowFactory.java
+++ src/main/java/com/azathorpe/cci/window/WindowFactory.java
@@ -1,5 +1,7 @@
 package com.azathorpe.cci.window;
 
+import com.azathorpe.cci.actions.utils.PersistenceStorage;
+import com.azathorpe.cci.service.SocketService;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
@@ -12,6 +14,10 @@ import org.jetbrains.annotations.NotNull;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * @author Azathorpe
+ * @version 1.0
+ */
 public class WindowFactory implements ToolWindowFactory, DumbAware {
     private JPanel mainPanel = new JPanel(new GridLayout(2,1));
     private JButton buttonCreateProblem = new JButton("Create Problem");
@@ -19,7 +25,11 @@ public class WindowFactory implements ToolWindowFactory, DumbAware {
 
     @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
+        //获取项目文件夹路径
+        PersistenceStorage.QUESTION_FOLDER_PATH = project.getBasePath() + "/.cci_questions/";
+        PersistenceStorage.QUESTION_SRC_FOLDER_PATH = project.getBasePath() + "/src/";
 
+        //设置按钮帮助信息
         mainPanel.add(buttonCreateProblem);
         mainPanel.add(buttonHelp);
         mainPanel.add(new JLabel(CommonDataKeys.PSI_FILE.getName()));
@@ -28,10 +38,23 @@ public class WindowFactory implements ToolWindowFactory, DumbAware {
         Content content = contentFactory.createContent(mainPanel, "", false);
 
         toolWindow.getContentManager().addContent(content);
+
+        //Load settings
+        PersistenceStorage.loadPropertiesFile();
+
+        prepare();
+    }
+
+    void prepare(){
+        //Auto fetch problems?
+        if(PersistenceStorage.settings.getAutoFetchProblems().equals("true")){
+            System.out.println("Auto fetch problems is enabled.");
+            SocketService.startServer();
+        }
     }
 
 
-    private void clearPanel(){
+    void clearPanel(){
         mainPanel.removeAll();
     }
 
