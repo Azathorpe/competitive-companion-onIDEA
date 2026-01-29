35de3af8937691c8c0680ce785271593a1f937d1
Azazel <384735481@qq.com>
2026-01-29 23:34:14 +0800

    [FEAT] add TemplateChecker

---
+++ src/main/java/com/azathorpe/cci/actions/utils/TemplateChecker.java
@@ -0,0 +1,25 @@
+package com.azathorpe.cci.actions.utils;
+
+import org.jetbrains.annotations.Nullable;
+
+public class TemplateChecker {
+    public static enum TemplateType {
+        PACKAGE_NAME,
+        FILE_NAME
+    }
+
+    /**
+     * Check which template is used in the given string.
+     * @param template Template String
+     * @return TemplateType or null if no template is found
+     */
+    public @Nullable TemplateType checkTemplate(String template) {
+        if (template.contains("${PACKAGE_NAME}")) {
+            return TemplateType.PACKAGE_NAME;
+        } else if (template.contains("${FILE_NAME}")) {
+            return TemplateType.FILE_NAME;
+        } else {
+            return null;
+        }
+    }
+}
