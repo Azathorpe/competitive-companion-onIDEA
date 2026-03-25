10611fb182fcbf2f61461f25888d16ce29f04cac
Azazel <384735481@qq.com>
2026-03-25 22:12:48 +0800

    [fix] 弄好了开局自动监听端口

--- src/main/java/com/azathorpe/cci/window/WindowFactory.java
+++ src/main/java/com/azathorpe/cci/window/WindowFactory.java
@@ -1,5 +1,7 @@
 package com.azathorpe.cci.window;
 
+import com.azathorpe.cci.service.SocketService;
+import com.azathorpe.cci.utils.Infos;
 import com.azathorpe.cci.utils.PersistentStorage;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
@@ -12,11 +14,12 @@ import java.awt.*;
 
 /**
  * 调整单独的Panel的地方
+ *
  * @author Azathorpe
  * @version 1.0
  */
 public class WindowFactory implements ToolWindowFactory, DumbAware {
-    private JPanel mainPanel = new JPanel(new GridLayout(2,1));
+    private JPanel mainPanel = new JPanel(new GridLayout(2, 1));
     private JButton buttonCreateProblem = new JButton("Create Problem");
     private JButton buttonHelp = new JButton("How to use this plugin?");
 
@@ -27,6 +30,13 @@ public class WindowFactory implements ToolWindowFactory, DumbAware {
 
     public static void initialization(Project project) {
         PersistentStorage.setBasePath(project.getBasePath());
+        //设置自动监听端口
+        Infos.updateSettings();
+        if (Infos.settings.getAutoFetchProblems().equals("true")) {
+            SocketService.startServer();
+        } else {
+            SocketService.stopServer();
+        }
     }
 
 }
